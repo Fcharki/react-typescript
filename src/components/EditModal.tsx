878eb0e24b9ad9002ca24a8d3f40ec3148c9d269
fadma charki <fadmaproco@gmail.com>
2025-03-16 02:16:03 +0000

    updated edit modal

--- src/components/EditModal.tsx
+++ src/components/EditModal.tsx
@@ -19,7 +19,7 @@ export const EditModal: FC<EditModalProps> = ({
   if (!showModal) return null;
   
   return (
-    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50  md:flex md:justify-center md:items-center  sm:flex sm:justify-center sm:items-center">
+<div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50 flex justify-center items-center lg:block">
       <div className="relative top-20 mx-auto p-5 border w-100 shadow-lg rounded-md bg-white">
         <div className="flex justify-between items-center border-b pb-3">
           <h3 className="text-xl font-semibold text-amber-900">Update Book</h3>
